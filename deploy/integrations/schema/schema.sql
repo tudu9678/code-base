8b2638137772165497588fb9eaef02a9bf72e688
Bonn <bonn@MacBook-Air-cua-Huynh.local>
2024-04-01 15:02:06 +0700

    implement api login

--- deploy/integrations/schema/schema.sql
+++ deploy/integrations/schema/schema.sql
@@ -3,14 +3,14 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE TABLE IF NOT EXISTS users  (
   id UUID PRIMARY KEY DEFAULT uuid_generate_v4(), -- Generate UUID v4
   full_name VARCHAR(255) NOT NULL,
-  phone_number VARCHAR(20) UNIQUE,
-  email VARCHAR(255) UNIQUE,
-  user_name VARCHAR(50) UNIQUE,
+  phone_number VARCHAR(20),
+  email VARCHAR(255),
+  user_name VARCHAR(50),
   password VARCHAR(255) NOT NULL,
   dob DATE,
   latest_login TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  CONSTRAINT check_login_credentials CHECK (phone_number IS NOT NULL OR email IS NOT NULL OR user_name IS NOT NULL)
+  CONSTRAINT check_register CHECK (phone_number IS NOT NULL OR email IS NOT NULL OR user_name IS NOT NULL)
 );
 
